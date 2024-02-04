4f91f0063427a7453986d1b31377146ef0ee8e9d
Dimas Pamungkas Mursidi <dmspmksmrsdi@gmail.com>
2024-02-04 10:48:11 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,57 @@
+/** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
+
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        inter: ["Inter", ...defaultTheme.fontFamily.sans],
+        montserrat: ["Montserrat", ...defaultTheme.fontFamily.sans],
+        poppins: ["Poppins", ...defaultTheme.fontFamily.sans],
+      },
+      colors: {
+        primary: "#DCDDFF",
+        secondary: "#6268D1",
+        tertiary: "#F6BE6A",
+      },
+      fontSize: {
+        "2xs": ".625rem",
+        "3.25xs": "13px",
+        12: "0.75rem",
+        14: "0.875rem",
+        15: "0,9rem",
+        20: "1.25rem",
+        24: "1.5rem",
+        28: "1.75rem",
+        32: "2rem",
+        36: "2.25rem",
+        40: "3rem",
+      },
+      spacing: {
+        0.75: "0.1875rem",
+        4.5: "1.125rem",
+        5.5: "1.375rem",
+      },
+      screens: {
+        xxs: "320px",
+        xs: "390px",
+        xsm: "480px",
+        mobile: "576px",
+        sm: "640px",
+        md: "768px",
+        "2md": "840px",
+        tablet: "960px",
+        lg: "1024px",
+        xl: "1200px",
+        "2xl": "1440px",
+        "3xl": "1536px",
+      },
+      animation: {
+        "spin-slow": "spin 1.5s linear infinite",
+        "spin-fast": "spin 0.75s linear infinite",
+      },
+    },
+  },
+  plugins: [],
+};
