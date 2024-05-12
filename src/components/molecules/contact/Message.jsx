fd9d643130671794a44e08617f09c76b9bff2501
Dimas Pamungkas Mursidi <dmspmksmrsdi@gmail.com>
2024-05-12 11:29:36 +0700

    Add form message

--- src/components/molecules/contact/Message.jsx
+++ src/components/molecules/contact/Message.jsx
@@ -3,8 +3,15 @@ import { MdOutlineMailOutline } from "react-icons/md";
 import { PiLinkedinLogo } from "react-icons/pi";
 import Button from "../../atoms/Button";
 import { Link } from "react-router-dom";
+import { useForm, ValidationError } from '@formspree/react';
 
 const Message = () => {
+
+  const [state, handleSubmit] = useForm("mpzvnpjb");
+  if (state.succeeded) {
+      return <div className="w-full h-40 max-w-2xl flex items-center justify-center p-6 bg-secondary text-primary rounded-md shadow-[4.0px_8.0px_8.0px_rgba(0,0,0,0.38)]"> <h1 className="font-inter text-center text-28 font-bold drop-shadow-[2px_2px_var(--tw-shadow-color)] shadow-black">Thanks for the message!</h1> </div>;
+  }
+
   return (
     <>
       <div className="w-full max-w-2xl flex flex-col py-6 bg-secondary text-primary rounded-md shadow-[4.0px_8.0px_8.0px_rgba(0,0,0,0.38)]">
@@ -43,31 +50,48 @@ const Message = () => {
           </nav>
         </div>
         {/* Form */}
-        <form action="" className="w-full flex flex-col gap-4 px-6 pb-4">
+        <form onSubmit={handleSubmit} action="" className="w-full flex flex-col gap-4 px-6 pb-4">
+            <fieldset id="fs-frm-inputs">
           <div className="flex flex-col smm:flex-row gap-4">
             <div className="flex flex-col smm:w-full smm:max-w-m">
-              <label htmlFor="Name" className="font-montserrat">
+              <label htmlFor="full-name" className="font-montserrat">
                 Name
               </label>
               <input
+                id="full-name"
                 type="text"
+                name="Name"
                 className="bg-primary border text-gray-900 text-sm rounded-lg outline-none w-full p-2.5"
                 placeholder="Ur Name..."
+                required=""
+              />
+              <ValidationError 
+                prefix="Name" 
+                field="name"
+                errors={state.errors}
               />
             </div>
             <div className="flex flex-col smm:w-full smm:max-w-2xl">
-              <label htmlFor="Name" className="font-montserrat">
-                Email
+              <label htmlFor="email-address" className="font-montserrat">
+                Your Email
               </label>
               <input
-                type="text"
+                id="email"
+                type="email"
+                name="Reply to"
                 className="bg-primary border text-gray-900 text-sm rounded-lg outline-none w-full p-2.5"
                 placeholder="example@gmail.com"
+                required=""
+              />
+              <ValidationError 
+                prefix="Email" 
+                field="email"
+                errors={state.errors}
               />
             </div>
           </div>
           <div className="flex flex-col">
-            <label htmlFor="Name" className="font-montserrat">
+            <label htmlFor="message" className="font-montserrat">
               Message
             </label>
             <textarea
@@ -77,13 +101,21 @@ const Message = () => {
               cols="30"
               rows="5"
               placeholder="Write ur message here.."
+              required=""
             ></textarea>
+            <ValidationError 
+              prefix="Message" 
+              field="message"
+              errors={state.errors}
+            />
           </div>
+            </fieldset>
           <Button
             variant="text-white w-3/4 smm:w-full self-center mt-2 -mb-4"
             border="border-primary"
             hover="hover:bg-primary hover:text-secondary"
             type="submit"
+            disabled={state.submitting}
           >
             Send
           </Button>
